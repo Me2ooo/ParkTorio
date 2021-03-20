334583913b79b91f42e0f5106888a085fe8026f2
Ivan Zhechev <danchovica69@gmail.com>
2021-03-20 19:52:02 +0000

    same line bars with date picker

--- my.js
+++ my.js
@@ -36,26 +36,3 @@ $(window).on("resize", function() {
 });
 
 
-$(function() {
-	$('#navbar.navbar-right ul li a').click(function() {
-	  //clear active status of any parent LI's
-	  $('#navbar.navbar-right ul li').removeClass('active');
-  
-	  // store id of new active sub-nav
-	  var currSub = $(this).parent();
-	  currSub.addClass('active')
-	  var id = currSub.attr('id');
-  
-	  // clear active status of any sub-nav list
-	  $('#subnavbar ul.navbar-nav').removeClass('active');
-  
-	  // set selected sub-nav to active
-	  $('.' + id).addClass('active');
-  
-	  console.log($('.' + id).attr('class'));
-  
-	});
-  
-  });
-
-
