b65e6e0ee81bb2bef082acf03647113e454045f3
Ivan Zhechev <danchovica69@gmail.com>
2021-03-20 20:39:40 +0000

    working individual bars

--- my.js
+++ my.js
@@ -1,38 +1,52 @@
-jQuery(function($) {
+jQuery(function ($) {
 	$('.progressCircle').circleProgress({
 		max: 100,
 		value: 69,
 		textFormat: 'percent',
 		animation: 'easeInOutQuad',
 	});
+
+	$(".jumbotron").css({ height: ($(window).height() * 0.75) + "px" });
+
+	$(window).on("resize", function () {
+		$(".jumbotron").css({ height: ($(window).height() * 0.75) + "px" });
+	});
+
+	var i;
+	for (i = 1; i < 19; i++) {
+		$.keyframe.define([{
+			name: 'progress' + i.toString(),
+			'100%': {
+				'height': i.toString() + '%',
+			}
+		},
+		]);
+	}
+
 });
 
-/* function progressBar(percent, $element) {
-    if(percent > 100){
-	percent = 100
-    }
+$(window).on("load", function () {
 
-    var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+	const numbers = [1, 2, 3, 4, 5];
+
+	var i; var j;
+	for (i = 6, j = 1; i < 24; i++, j++) {
+		console.log(j);
+		$('#' + i.toString() + 'am').playKeyframe({
+			name: 'progress' + j.toString(),
+			duration: '1.75s',
+			fillMode: 'forwards',
+		})
+	}
+});
+/* function progressBar(percent, $element) {
+	if(percent > 100){
+	percent = 100
+	}
+
+	var progressBarWidth = percent * $element.width() / 100;
+	$element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
 } */
 
-$(window).scroll(function (){
-	var scroll = $(window).scrollTop();
-
-	if (scroll >=10) {
-		$('.w3-top').removeClass('hidden');
-
-	}
-	else {
-		$('.w3-top').addClass('hidden');
-	}
-});
-
-
-$(".jumbotron").css({ height: ($(window).height()*0.75) + "px" });
-
-$(window).on("resize", function() {
-  $(".jumbotron").css({ height: ($(window).height()*0.75) + "px" });
-});
 
 
