798975196d44880337ffd6c47af6998c737406bb
Ivan Zhechev <danchovica69@gmail.com>
2021-03-20 11:02:17 +0000

    wip progress bars

--- my.js
+++ my.js
@@ -1,8 +1,17 @@
 jQuery(function($) {
-    $('.progress').circleProgress({
+    $('.progressCircle').circleProgress({
     max: 100,
 	value: 69,
     textFormat: 'percent',
     //animation: 'easeInOutCirc',
     });
   });
+
+ /* function progressBar(percent, $element) {
+    if(percent > 100){
+        percent = 100
+    }
+
+    var progressBarWidth = percent * $element.width() / 100;
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+} */
