10ecd4f3e5ce97b109b5eba33fcdba62f34d33b2
Ivan Zhechev <danchovica69@gmail.com>
2021-03-20 15:18:34 +0000

    starting to work on getting the bars aligned

--- my.js
+++ my.js
@@ -1,18 +1,32 @@
 jQuery(function($) {
-    $('.progressCircle').circleProgress({
-    max: 100,
-	value: 69,
-    textFormat: 'percent',
-    //animation: 'easeInOutCirc',
-    });
-  });
+	$('.progressCircle').circleProgress({
+		max: 100,
+		value: 69,
+		textFormat: 'percent',
+		animation: 'easeInOutQuad',
+	});
+});
 
- /* function progressBar(percent, $element) {
+/* function progressBar(percent, $element) {
     if(percent > 100){
-        percent = 100
+	percent = 100
     }
 
     var progressBarWidth = percent * $element.width() / 100;
     $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
 } */
+
+$(window).scroll(function (){
+	var scroll = $(window).scrollTop();
+
+	if (scroll >=10) {
+		$('.w3-top').removeClass('hidden');
+
+	}
+	else {
+		$('.w3-top').addClass('hidden');
+	}
+});
+
+
 
